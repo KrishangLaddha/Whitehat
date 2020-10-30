e5f824325480dedb33581da2e01e838ed5992ec9
KrishangLaddha <72633705+KrishangLaddha@users.noreply.github.com>
2020-10-30 18:55:42 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,35 @@
-var fixedRect, movingRect
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world, ground, ball;
+
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600,400,50,80);
-  fixedRect.shapeColor = "green";
-  movingRect = createSprite(400,20,80,30);
-  movingRect.shapeColor = "green";
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options = {
+    isStatic: true
+  }
+   
+ground = Bodies.rectangle(200,390,200,20, ground_options);
+World.add(world,ground);
+var ball_options = {
+ restitution: 1.5    
+}
+ball = Bodies.circle(200,100,20,ball_options);  
+World.add(world,ball)
+
 }
 
 function draw() {
-  background(255,255,255);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 
-    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2 ){
-movingRect.shapeColor = "red";
-fixedRect.shapeColor = "red";
-  }
-  else{
-  movingRect.shapeColor = "green";
-  fixedRect.shapeColor = "green";
-}
-  drawSprites();
+  background(0);  
+  Engine.update(engine);
+rectMode(CENTER) ;
+rect(ground.position.x,ground.position.y,400,20);
+ellipseMode(RADIUS)
+ellipse(ball.position.x,ball.position.y,20,20)
 }
